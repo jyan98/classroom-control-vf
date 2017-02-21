bec5025891d76e7b73f26f661b7cf9cab2993add
The Prez <ryan+puppet@theryantist.com>
2017-02-21 16:17:04 -0600

    add skeleton module

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,17 @@
+class skeleton{
+  file { '/etc/skel':
+    ensure => 'directory',
+    group  => '0',
+    mode   => '0755',
+    owner  => '0',
+    type   => 'directory',
+  }
+  file { '/etc/skel/.bashrc':
+    ensure  => 'file',
+    group   => '0',
+    mode    => '0644',
+    owner   => '0',
+    type    => 'file',
+    source => 'puppet:///skeleton/files/bashrc
+  }
+}
