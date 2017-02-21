59b2e419b9c6ce0bdfe1adafd60137ebf8a9331d
The Prez <ryan+puppet@theryantist.com>
2017-02-21 14:00:09 -0600

    add file resource

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,8 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  exec {'cowsay-motd':
-    command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
-    creates => '/etc/motd',
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "WELCOME TO EVERYTHING.\n",
   }
 }
