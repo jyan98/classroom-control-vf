3e5430943567e2d2e8abad75e7be85d6ee90011f
The Prez <ryan+puppet@theryantist.com>
2017-02-21 16:21:41 -0600

    include skeleton module

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,7 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   include users
+  include skeleton
   exec {'cowsay-motd':
     command => "cowsay \'Welcome to ${::fqdn}!\' > /etc/motd",
     creates => '/etc/motd',
