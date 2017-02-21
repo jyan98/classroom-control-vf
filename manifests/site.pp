73033fed26994f63fd181b96f3fe3d01550f4797
michaellukach <lmike6453@hotmail.com>
2017-02-21 14:09:07 -0500

    Added: motd file / cowsay package

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,3 +45,15 @@ node default {
   include role::classroom
 }
 # Added blank comment
+file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Hey, Puppet is fun!\n",
+}
+
+package { 'cowsay':
+  ensure   => present,
+  provider => gem,
+}
