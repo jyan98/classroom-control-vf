38de95bc4977a9666b5ab2555c6d81b20eed94c1
mhaverkamp <mhaverkamp@kcp.com>
2017-02-21 14:58:31 -0600

    Added class

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,8 +1,7 @@
-user { 'fundamentals':
-  ensure           => 'present',
-  home             => '/home/fundamentals',
-  password         => '!!',
-  password_max_age => '99999',
-  password_min_age => '0',
-  shell            => '/bin/bash',
+# $modulepath/users/manifests/init.pp
+
+class users {
+  user { 'fundamentals':
+    ensure           => 'present',
+  }
 }
