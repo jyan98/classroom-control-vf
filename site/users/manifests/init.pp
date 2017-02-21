8e62cda3781affb8666f4b545e19cc73362dd35d
mhaverkamp <mhaverkamp@kcp.com>
2017-02-21 14:55:02 -0600

    Create init.pp

---
+++ site/users/manifests/init.pp
@@ -0,0 +1,8 @@
+user { 'fundamentals':
+  ensure           => 'present',
+  home             => '/home/fundamentals',
+  password         => '!!',
+  password_max_age => '99999',
+  password_min_age => '0',
+  shell            => '/bin/bash',
+}
