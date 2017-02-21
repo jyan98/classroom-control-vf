7b7c6d363dc3e3c930eb3f46b5971487d9b885e0
mhaverkamp <mhaverkamp@kcp.com>
2017-02-21 14:28:22 -0600

    Exercise 7.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,11 +43,15 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  file { '/etc/motd':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "Hello World!\n",
+#  file { '/etc/motd':
+#    ensure  => file,
+#    owner   => 'root',
+#    group   => 'root',
+#    mode    => '0644',
+#    content => "Hello World!\n",
+#  }
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    path => '/usr/local/bin',
+    creates => '/etc/motd',
   }
 }
