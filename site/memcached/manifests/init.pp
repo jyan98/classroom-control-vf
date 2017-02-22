9507c77c46cca3372b29f0eaecafefff9ec5256c
dlschieber <dlschieber@hotmail.com>
2017-02-22 11:02:32 -0600

    create memcached init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,17 @@
+class memcached {
+  package { 'memcached':
+    ensure => present,
+  }
+  
+  file { '/etc/sysconfig/memcached':
+    ensure => present,
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+    notify => Service['memcached'],
+  }
+  
+  service { 'memcached':
+    ensure=>running,
+    require => File['/etc/sysconfig/memcached'],
+  }
+}
