ec373d965fb84be0b081b029148c02587c326bd1
dlschieber <dlschieber@hotmail.com>
2017-02-22 12:19:19 -0600

    cleanup

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -5,6 +5,7 @@ class memcached {
   
   file { '/etc/sysconfig/memcached':
     ensure => present,
+    owner => 'root',
     source  => 'puppet:///modules/memcached/memcached',
     require => Package['memcached'],
     notify => Service['memcached'],
@@ -13,6 +14,5 @@ class memcached {
   service { 'memcached':
     ensure => running,
     enable => true,
-    require => File['/etc/sysconfig/memcached'],
   }
 }
