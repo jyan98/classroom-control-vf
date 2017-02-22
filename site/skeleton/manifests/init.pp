ab3ecba61d9ed078cc168cfda8cb2bf3c8303701
michaellukach <lmike6453@hotmail.com>
2017-02-22 10:09:20 -0500

    Added: skeleton module init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,10 @@
+$modulepath/skeleton/manifests/init.pp
+class skeleton {
+    file { '/etc/skel':
+    ensure => 'directory',
+    }
+    file { '/etc/skel/bashrc':
+    ensure => 'file',
+    source => 'puppet:///modules/skeleton/bashrc
+    }
+}
