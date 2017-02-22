7559f26e6a9c03d14b1edd0f70e65adc222ba691
michaellukach <lmike6453@hotmail.com>
2017-02-22 14:16:37 -0500

    Added: nginx class declaration to default node classification

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
 #  include role::classroom
-#  include nginx
+   include nginx
    include memcached
 #  file { '/etc/motd':
 #  ensure  => file,
