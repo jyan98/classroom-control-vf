2b3d621b964be4713f1ce356a90a8c50985534eb
michaellukach <lmike6453@hotmail.com>
2017-02-22 12:09:22 -0500

    Added memcached init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,14 @@
+
+package { 'memcached':
+  ensure => 'present',
+}
+file { '/etc/sysconfig/memcached':
+  ensure => 'present',
+  source => 'puppet:///memcached/files/memcached',
+  require => Package['memcached'],
+  notify => Service['memcached'],
+}
+service { 'memcached':
+  ensure => 'started',
+  enable => 'true',
+}
