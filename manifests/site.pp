4042b7590db4b1794c658725a06f0546656fcbad
zenkpr <kp.rajesh.kp@gmail.com>
2017-02-22 21:39:07 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,5 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
-  include users
-  include skeleton
+  
   }
