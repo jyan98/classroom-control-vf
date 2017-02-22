78e665e8ae6dee2b30581d6a939ea2357321ceac
michaellukach <lmike6453@hotmail.com>
2017-02-22 14:36:50 -0500

    moving init.pp to correct location

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,36 @@
+# $modulepath/nginx/manifests/init.pp
+class nginx {
+  File {
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+  }
+  
+  file { '/var/www/index.html':
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/conf.d/default.conf':
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  
+  service { 'nginx':
+    ensure    => running,
+    subscribe => [File['/etc/nginx/nginx.conf'],File['/etc/nginx/conf.d/default.conf']],
+  }
+}
