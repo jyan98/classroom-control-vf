2c53eecb87f6f74bdb9613acd2b25d987ee46f01
yavin06 <domdalessandro@yahoo.com>
2017-02-22 14:18:13 -0500

    Setting default permissions and removing local

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,5 +1,10 @@
 # $modulepath/nginx/manifests/init.pp
-
+File {
+  group => 'root',
+  owner => 'root',
+  mode  => '0644',
+  }
+  
 class nginx {
   package { 'nginx': 
     ensure => 'present',
@@ -9,23 +14,14 @@ class nginx {
     }
   file { '/etc/nginx/nginx.conf':
     ensure => 'file',
-    group => 'root',
-    owner => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => 'file',
-    group => 'root',
-    owner => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     }
   file { '/var/www/index.html':
     ensure => 'file',
-    group => 'root',
-    owner => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
     }
   service { 'nginx':
