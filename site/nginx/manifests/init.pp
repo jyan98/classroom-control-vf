91781910b49d2c4cd7bcec80d351f07639bddd44
malex133 <malex@mmalexander.com>
2017-02-22 09:32:15 -0600

    updated with files config and directory tests

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,5 +4,36 @@ class nginx_class_demo {
   package { 'nginx':
    ensure => 'installed',
   }
-  
+  service { 'nginx':
+    ensure => 'running',
+    enable => 'true',
+}
+file { '/var/www':
+  ensure => 'directory',
+}
+file { '/etc/nginx':
+  ensure => 'directory',
+}
+
+file { '/etc/nginx/conf.d/default.conf':
+  ensure => 'file',
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  source  => 'puppet:///modules/nginx/default.conf',
+}
+file { '/etc/nginx/nginx.conf':
+  ensure => 'file',
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  source  => 'puppet:///modules/nginx/nginx.conf',
+ } 
+ file { '/var/www/index.html':
+  ensure => 'file',
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  source  => 'puppet:///modules/nginx/index.html',
+ } 
 }
