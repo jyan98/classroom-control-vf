4510b45535d117bef281bf86d780f5273a12983c
seadoox <aaron.c.norton@hotmail.com>
2017-02-22 14:52:47 -0600

    changing to use erb

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -7,7 +7,7 @@ class aliases (
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => epp('aliases/aliases.epp', { admin => $admin }),
+        content => erb('aliases/aliases.erb', { admin => $admin }),
     }
     exec { '/usr/bin/newaliases':
         refreshonly => true,
