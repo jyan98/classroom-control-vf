ee15fff536fa1d6ec006256b103194a4d3f367cd
helgesoj <jack.helgeson@kellogg.com>
2017-02-22 13:27:00 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,5 +1,6 @@
 class nginx {
 File {
+ensure => file,
 owner => 'root',
 group => 'root',
 mode => '0664',
@@ -11,17 +12,14 @@ file { [ '/var/www', '/etc/nginx/conf.d' ]:
 ensure => directory,
 }
 file { '/var/www/index.html':
-ensure => file,
 source => 'puppet:///modules/nginx/index.html',
 }
 file { '/etc/nginx/nginx.conf':
-ensure => file,
 source => 'puppet:///modules/nginx/nginx.conf',
 require => Package['nginx'],
 notify => Service['nginx'],
 }
 file { '/etc/nginx/conf.d/default.conf':
-ensure => file,
 source => 'puppet:///modules/nginx/default.conf',
 notify => Service['nginx'],
 require => Package['nginx'],
