f098cc109d719507fc20d29a79177266f5c45f36
michaellukach <lmike6453@hotmail.com>
2017-02-22 10:19:10 -0500

    Fixed .bashrc absolute path

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -3,7 +3,7 @@ class skeleton {
     file { '/etc/skel':
     ensure => 'directory',
     }
-    file { '/etc/skel/bashrc':
+    file { '/etc/skel/.bashrc':
     ensure => 'file',
     source => 'puppet:///modules/skeleton/bashrc',
     }
