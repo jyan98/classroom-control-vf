e08a27276a641539d3e26a97f0d4166fcdaf8b27
t010214 <tamalee_delamater@newyorklife.com>
2017-02-22 10:38:38 -0500

    added file resources

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,17 +1,35 @@
+# $modulepath/site/nginx/manifests
 class nginx {
   package {'nginx',
   ensure => present,
   }
  file {'/var/www' :
-  ensure => file,
+  ensure => directory,
   owner  => 'root',
   group  => 'root',
   mode  =>  '0755',
   }
+  
+file { '/var/www/index.html':
+ ensure => file,
+ owner => 'root',
+ group => 'root',
+ mode => '0664',
+ source => 'puppet:///modules/nginx/index.html',
+ }
+
  file {'/etc/nginx/ngxin.conf':
   ensure => file,
   owner => 'root',
   group => 'root'
-  mode => '0755'
-  source => '/etc/nginx/conf.d/default.conf',
+  mode => '0644'
+  source => 'puppet:///modules.nginx/nxgin.conf',
+ }
+ 
+ file {'/etc/nginx/conf.d':
+  ensure=> directory,
+  owner => 'root'
+  group => 'root'
+  mode  => '0775'
+  source => 'puppet:///modules/nginx/default.conf',
   }
