9871de98677dcf6dc0c4a334c1192df0acde6a3e
paponte14 <paponte14@gmail.com>
2017-02-22 14:32:02 -0500

    Update init.pp

--- site/nginx/files/manifests/init.pp
+++ site/nginx/files/manifests/init.pp
@@ -3,12 +3,14 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
+  
   file { '/var/www':
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0775',
   }
+  
   file { '/var/www/index.html':
     ensure => file,
     owner  => 'root',
@@ -16,6 +18,7 @@ class nginx {
     mode   => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
+  
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
     owner   => 'root',
@@ -25,12 +28,14 @@ class nginx {
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
+  
   file { '/etc/nginx/conf.d':
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0775',
   }
+  
   file { '/etc/nginx/conf.d/default.conf':
     ensure  => file,
     owner   => 'root',
@@ -40,6 +45,7 @@ class nginx {
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
+  
   service { 'nginx':
     ensure    => running,
     enable    => true,
