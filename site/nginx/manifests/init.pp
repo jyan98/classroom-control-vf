797298e28916ac45f81fb7d55022034230d01e31
The Prez <ryan+puppet@theryantist.com>
2017-02-21 22:42:51 -0600

    add file resources to nginx class

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,4 +2,33 @@ class nginx{
   package { 'nginx':
     ensure => present,
   }
+  service { 'nginx':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure  => 'file',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    notify  => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => 'file',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/nginx/default.conf',
+    notify  => Service['nginx'],
+  }
+  file { '/var/www/index.html':
+    ensure  => 'file',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/nginx/index.html',
+  }
 }
