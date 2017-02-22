a808c2bf749b6ded60ccc3ad907b5371bad49ad6
seadoox <aaron.c.norton@hotmail.com>
2017-02-22 09:09:31 -0600

    initial skel bashrc

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,14 @@
+# $module/skeleton/manifests/init.pp
+class skeleton {
+  file { '/etc/skel':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+  }
+  file { '/etc/skel/.bashrc':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    source => "puppet:///modules/${module_name}/bashrc",
+  }
+}
