35fba25531488ba5e9b4cb1cbac8c2634a1a2fb7
seadoox <aaron.c.norton@hotmail.com>
2017-02-22 15:31:22 -0600

    forgot to close curly brace

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,7 +2,7 @@ class nginx {
   case $facts['os']['family'] {
     'RedHat': { $document_root = '/var/www' }
     'Windows': { $document_root = 'C/ProgramData/nginx/html' }
-    default: { fail("Operating system family ${facts['os']['family']} is not supported.")
+    default: { fail("Operating system family ${facts['os']['family']} is not supported.") }
   }
   File {
     owner => 'nginx',
