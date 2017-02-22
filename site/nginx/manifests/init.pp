6c9f326f6dcdb39fa7f79e870a79607b4b1853fc
malex133 <malex@mmalexander.com>
2017-02-22 13:16:27 -0600

    create File with defaults

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,6 +1,12 @@
 # init.pp file for demo
 
 class nginx {
+
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   package { 'nginx':
    ensure => 'installed',
   }
@@ -17,23 +23,14 @@ file { '/etc/nginx':
 
 file { '/etc/nginx/conf.d/default.conf':
   ensure => 'file',
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
   source  => 'puppet:///modules/nginx/default.conf',
 }
 file { '/etc/nginx/nginx.conf':
   ensure => 'file',
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
   source  => 'puppet:///modules/nginx/nginx.conf',
  } 
  file { '/var/www/index.html':
   ensure => 'file',
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
   source  => 'puppet:///modules/nginx/index.html',
  } 
 }
