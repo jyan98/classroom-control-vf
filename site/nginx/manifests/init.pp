11c72a8ee275a3984928170d6f33f05625c25dac
seadoox <aaron.c.norton@hotmail.com>
2017-02-22 06:50:24 -0600

    initial nginx module config

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,44 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure => 'directory',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+  
+  file { '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/${module}/index.html',
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['nginx'],
+    source => 'puppet:///modules/${module}/nginx.conf',
+  }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/${module}/default.conf',
+  }
+  
+  service {'nginx':
+    ensure => running,
+    enable => true,
+    require => File['/etc/nginx/nginx.conf'],
+  }
+  
+}
