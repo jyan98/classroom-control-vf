b7362aad60d6ab6927da208ea0c9527678fddf31
Laura Macchi <lau.macchi@gmail.com>
2017-02-22 10:17:16 -0600

    Remove existing classification

--- manifests/site.pp
+++ manifests/site.pp
@@ -36,21 +36,10 @@ ini_setting { 'random ordering':
 # The default node definition matches any node lacking a more specific node
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
-node /lmacchi/ {
-  notify { "This is a regex match": }
-}
-
-node 'lmacchi.puppetlabs.vm' {
-  notify { "This is an exact match": }
-}
+# specified in the console for that node
 
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
-  include nginx
-  notify { "This is a default match": }
 }
