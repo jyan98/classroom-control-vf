46f3ffb57ca9181facc2181031de28bfd858d015
michaellukach <lmike6453@hotmail.com>
2017-02-22 15:12:24 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,6 +38,10 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+if $facts['is_virtual'] {
+  warning('This is a virtual machine')
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
