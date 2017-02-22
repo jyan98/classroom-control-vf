65ca8c3e57640da452e62a8713b07cf1f8e5b233
yavin06 <domdalessandro@yahoo.com>
2017-02-22 14:25:38 -0500

    moved defaults into class

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,27 +1,28 @@
 # $modulepath/nginx/manifests/init.pp
-File {
+
+  
+class nginx {
+  File {
+  ensure => 'file',
   group => 'root',
   owner => 'root',
   mode  => '0644',
   }
   
-class nginx {
   package { 'nginx': 
     ensure => 'present',
     }
+  
   file { '/var/www':
     ensure => 'directory',
     }
   file { '/etc/nginx/nginx.conf':
-    ensure => 'file',
     source => 'puppet:///modules/nginx/nginx.conf',
     }
   file { '/etc/nginx/conf.d/default.conf':
-    ensure => 'file',
     source => 'puppet:///modules/nginx/default.conf',
     }
   file { '/var/www/index.html':
-    ensure => 'file',
     source => 'puppet:///modules/nginx/index.html',
     }
   service { 'nginx':
