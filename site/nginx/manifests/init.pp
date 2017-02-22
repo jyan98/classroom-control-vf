4c505d761754d84b0c086ac7d90a28e7def5bc59
mhaverkamp <mhaverkamp@kcp.com>
2017-02-22 13:17:21 -0600

    Use defaults

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,48 +1,35 @@
 class nginx {
+  File {
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+  
   package { 'nginx':
     ensure => present,
   }
   
   file { '/var/www':
     ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
   file { '/var/www/index.html':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx':
     ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     notify  => Service['nginx'],
   }
   
   file { '/etc/nginx/conf.d':
     ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
   file { '/etc/nginx/conf.d/default.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/default.conf',
     notify  => Service['nginx'],
   }
