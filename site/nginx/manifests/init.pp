d924917f70c15b60b9a5d638fa1a781afc70fd29
dlschieber <dlschieber@hotmail.com>
2017-02-22 09:44:04 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -30,7 +30,7 @@ class nginx {
     group   => 'root',
     mode    => '0644',
 #    source  => 'puppet:///modules/nginx/nginx.conf'
-    source  => 'puppet:///modules/nginx/nginx.conf'
+    source  => 'puppet:///modules/nginx/nginx.conf',
 #    content => file("${module_name}/nginx.conf"),
     notify  => Service['nginx'],
   }
@@ -46,8 +46,8 @@ class nginx {
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => file("${module_name}/default.conf"),
-#    source  => 'puppet:///modules/nginx/default.conf'
+#    content => file("${module_name}/default.conf"),
+    source  => 'puppet:///modules/nginx/default.conf',
     notify  => Service['nginx'],
   }
   
