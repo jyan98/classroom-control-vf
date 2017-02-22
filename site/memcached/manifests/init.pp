b5b7e8172e1196cd46e98c164be8f45d32a6ef05
michaellukach <lmike6453@hotmail.com>
2017-02-22 12:14:11 -0500

    Modified: Wrapped resource types within main memcached class

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,14 +1,16 @@
-
-package { 'memcached':
-  ensure => 'present',
-}
-file { '/etc/sysconfig/memcached':
-  ensure => 'present',
-  source => 'puppet:///memcached/files/memcached',
-  require => Package['memcached'],
-  notify => Service['memcached'],
-}
-service { 'memcached':
-  ensure => 'started',
-  enable => 'true',
+# $modulepath/memcached/manifests/init.pp
+class memcached {
+  package { 'memcached':
+    ensure => 'present',
+  }
+  file { '/etc/sysconfig/memcached':
+    ensure => 'present',
+    source => 'puppet:///memcached/files/memcached',
+    require => Package['memcached'],
+    notify => Service['memcached'],
+  }
+  service { 'memcached':
+    ensure => 'started',
+    enable => 'true',
+  }
 }
