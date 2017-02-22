3e61b72622268a0e8dbaca9c5879b32005855eeb
zenkpr <kp.rajesh.kp@gmail.com>
2017-02-22 20:05:59 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,12 +43,5 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  file { '/etc/motd' :
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "puppet puppet puppet......\n",
   include users
   }
-}
