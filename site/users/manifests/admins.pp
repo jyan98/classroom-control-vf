4e447a0a15f54e96bedcccc92465126efea35bfb
helgesoj <jack.helgeson@kellogg.com>
2017-02-23 06:05:57 -0600

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,11 +1,19 @@
 class users::admins {
 users::managed_user { 'jack': }
+users::managed_user { 'jose': }
+
 users::managed_user { 'lynne':
 group => 'staff',
 }
-users::managed_user { 'kieth':
+users::managed_user { 'alice':
+group => 'staff'
+}
+
+users::managed_user { 'chen':
 group => 'staff',
 }
+
+
 group { 'staff':
 ensure => present,
 }
